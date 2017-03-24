54ec3d01b9e78ebae277a9ff439afc087993717f
Josh Bassett <josh.bassett@gmail.com>
2017-03-24 20:09:28 +1100

    Refactor to remove zeromq and use iron http

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,39 @@
+/*
+ * Eventlog Daemon
+ * Joshua Bassett, 2017
+ */
+
+extern crate env_logger;
+#[macro_use] extern crate log;
+#[macro_use] extern crate router;
 extern crate rustc_serialize;
-extern crate time;
-extern crate zmq;
 
 use rustc_serialize::json::{self};
 use std::cmp;
 use std::fs::File;
 use std::fs::OpenOptions;
-use std::io::{BufReader, LineWriter, SeekFrom, Error};
+use std::io::{BufReader, LineWriter, Error};
 use std::io::prelude::*;
-use std::thread;
+use std::sync::mpsc::channel;
 
-#[derive(RustcDecodable, RustcEncodable)]
-struct Entry {
-	id: u32,
-	timestamp: String,
-	payload: String
-}
+mod command;
+mod entry;
+mod server;
+mod uploader;
+
+use command::Command::WriteEntry;
+use entry::Entry;
+use server::spawn_server;
+use uploader::spawn_uploader;
 
 // Return the next ID in the journal.
 fn get_next_id() -> Result<u32, Error> {
-    let file = try!(File::open("journal.txt"));
+    let file = File::open("journal.txt")?;
     let mut reader = BufReader::new(file);
     let mut buffer = String::new();
     let mut id = 0;
 
-    while try!(reader.read_line(&mut buffer)) > 0 {
+    while reader.read_line(&mut buffer)? > 0 {
         let entry: Entry = json::decode(&buffer).unwrap();
         id = cmp::max(entry.id, id);
         buffer.clear();
@@ -33,61 +42,37 @@ fn get_next_id() -> Result<u32, Error> {
     Ok(id + 1)
 }
 
-// Write journal entry.
-fn write_entry<W: Write>(writer: &mut LineWriter<W>, id: u32, payload: String) -> Result<(), Error> {
-    let timestamp = time::strftime("%Y-%m-%d %H:%M:%S.%f", &time::now()).unwrap();
-
-    let entry = Entry {
-        id: id,
-        timestamp: timestamp,
-        payload: payload
-    };
-
-    writeln!(writer, "{}", json::encode(&entry).unwrap())
+// Write a journal entry using the given writer.
+fn write_entry_to_journal<W: Write>(writer: &mut LineWriter<W>, entry: &Entry) -> Result<(), Error> {
+    writeln!(writer, "{}", json::encode(entry).unwrap())
 }
 
-// Read entries from journal and post events to S3.
-fn reader_thread() -> Result<(), Error> {
-    let mut file = try!(File::open("journal.txt"));
+fn run() -> Result<(), Error> {
+    info!("Starting up...");
 
-    // Seek to the end of the file.
-    try!(file.seek(SeekFrom::End(0)));
+    let mut options = OpenOptions::new();
+    let file = options.append(true).open("journal.txt")?;
+    let mut writer = LineWriter::new(file);
+    let next_id = get_next_id()?;
+    let (server_tx, server_rx) = channel();
+    let (uploader_tx, uploader_rx) = channel();
 
-    let mut reader = BufReader::new(file);
-    let mut buffer = String::new();
+    spawn_server(server_tx, next_id)?;
+    spawn_uploader(uploader_rx)?;
 
+    // Receive message from server, journal it, and send to uploader.
     loop {
-        while try!(reader.read_line(&mut buffer)) > 0 {
-            print!("{}", buffer);
-            buffer.clear();
+        let command = server_rx.recv().unwrap();
+        match command {
+            WriteEntry(entry) => {
+                write_entry_to_journal(&mut writer, &entry)?;
+                uploader_tx.send(entry).unwrap();
+            }
         }
     }
 }
 
-fn journal_thread(mut id: u32) -> Result<(), Error> {
-    let mut options = OpenOptions::new();
-    let file = try!(options.append(true).open("journal.txt"));
-    let mut writer = LineWriter::new(file);
-
-    let context = zmq::Context::new();
-    let broker = context.socket(zmq::REP).unwrap();
-
-    assert!(broker.bind("tcp://*:5678").is_ok());
-
-    loop {
-        let payload = broker.recv_string(0).unwrap().unwrap();
-        try!(write_entry(&mut writer, id, payload));
-        broker.send(b"", 0).unwrap();
-        id += 1;
-    }
-}
-
 fn main() {
-    let id = get_next_id().unwrap();
-
-    thread::spawn(|| {
-        reader_thread().unwrap();
-    });
-
-    journal_thread(id).unwrap();
+    env_logger::init().unwrap();
+    run().unwrap();
 }
